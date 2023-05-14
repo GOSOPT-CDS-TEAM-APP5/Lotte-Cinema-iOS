d6e1bf9b14f159b416d335fec9759af9b740546a
parkikbum <pibum@naver.com>
2023-05-15 02:04:03 +0900

    add: imageLiterals & add image asset

---
+++ Lotte-Cinema/Lotte-Cinema/Resource/Literals/ImageLiterals.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+enum ImageLiterals {
+    static var icCheck: UIImage { .load(name: "ic_check") }
+    static var icChevronRight: UIImage { .load(name: "ic_chevron_right") }
+    static var icClose: UIImage { .load(name: "ic_close") }
+    static var icEvent: UIImage { .load(name: "ic_event") }
+    static var icGrayHeart: UIImage { .load(name: "ic_heart_3") }
+    static var icFillHeart: UIImage { .load(name: "ic_heart_2_filled") }
+    static var icUnFillHeart: UIImage { .load(name: "ic_heart_2_unfilled") }
+    static var icShakePhone: UIImage { .load(name: "ic_shake_phone") }
+    static var icShakeTicket: UIImage { .load(name: "ic_shake_ticket") }
+    static var icShare: UIImage { .load(name: "ic_share") }
+    static var icStar: UIImage { .load(name: "ic_star") }
+    static var iconMenu: UIImage { .load(name: "icon_menu") }
+    static var iconMypage: UIImage { .load(name: "icon_mypage") }
+    static var iconPopcorn: UIImage { .load(name: "icon_popcorn") }
+    static var iconTicket: UIImage { .load(name: "icon_ticket") }
+    
+}
---
+++ Lotte-Cinema/Lotte-Cinema/Source/Extension/UIImage+.swift
@@ -0,0 +1,11 @@
+import UIKit
+extension UIImage {
+    static func load(name: String) -> UIImage {
+        guard let image = UIImage(named: name, in: nil, compatibleWith: nil) else {
+            return UIImage()
+        }
+        image.accessibilityIdentifier = name
+        return image
+    }
+    
+}
