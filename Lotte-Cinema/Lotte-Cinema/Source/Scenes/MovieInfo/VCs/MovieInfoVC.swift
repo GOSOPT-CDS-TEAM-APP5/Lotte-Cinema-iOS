c0ddffcebc03c699b57a7267d3f6d49ef9e74c24
parkikbum <pibum@naver.com>
2023-05-15 18:10:49 +0900

    feat #2: 영화 정보 뷰 컨트롤러 추가

--- Lotte-Cinema/Lotte-Cinema/Source/Scenes/MovieInfo/VCs/MovieInfoVC.swift
+++ Lotte-Cinema/Lotte-Cinema/Source/Scenes/MovieInfo/VCs/MovieInfoVC.swift
@@ -1,29 +1,51 @@
-//
-//  MovieInfoVC.swift
-//  Lotte-Cinema
-//
-//  Created by 박익범 on 2023/05/15.
-//
-
 import UIKit
 
 class MovieInfoVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.setLayout()
+        self.setTableViewConfig()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: - Configuration
+    private func setTableViewConfig() {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        let headerView = InfoTableViewHeader()
+        self.tableView.tableHeaderView = headerView
     }
-    */
+    
+    //MARK: - Layout
+    private func setLayout() {
+        self.view.backgroundColor = .red
+        self.navigationController?.navigationBar.isHidden = true
+        self.view.addSubviews(tableView, navigationView)
+        navigationView.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+            $0.height.equalTo(90)
+        }
+        tableView.snp.makeConstraints {
+            $0.top.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+    
+    //MARK: - Components
+    private let navigationView = MovieInfoNavigationView()
+    private let tableView = UITableView(frame: .zero, style: .grouped).then {
+        $0.backgroundColor = .black
+        $0.contentInsetAdjustmentBehavior = .never
+    }
 
+}
+
+extension MovieInfoVC: UITableViewDelegate {}
+extension MovieInfoVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 }
